5ed27ecde0a89ff3655fc0e7801f8b373e1a0899
Pawar-Sudharshan <pawarsudharshan47@gmail.com>
2026-01-25 17:22:06 +0530

    dashboard and adding account done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <main className="min-h-screen bg-white pt-16">
             {children}
           </main>
+          <Toaster richColors />
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with 💗 by Sudharshan</p>
